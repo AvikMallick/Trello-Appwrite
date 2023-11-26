9c8056d71f0316ae091107787579511d72c7f013
Avik Mallick <avikmallick111101@gmail.com>
2023-11-27 00:06:23 +0530

    till 4:13:51

---
+++ app/api/generateSummary/route.ts
@@ -0,0 +1,38 @@
+import openai from '@/openai';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  // todos in the body of the post req
+  const { todos } = await request.json();
+  console.log(todos);
+
+  // communicate with openAI GPT
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: 'system',
+        content:
+          'When responding, welcome user always as Mr. or Ms. Awesome and say welcome to the Trello 2.0 App!',
+      },
+      {
+        role: 'user',
+        content: `Hi there, provide a summary of the following todos. Count how many todos are in each category such as To Do, In Progress, and Done, then tell the user to have a productive day! Here's the data: ${JSON.stringify(
+          todos
+        )}`,
+      },
+    ],
+  });
+
+  console.log(response);
+
+  const { data } = response;
+
+  console.log('DATA IS: ', data);
+  console.log(data.choices[0].message);
+
+  return NextResponse.json(data.choices[0].message);
+}
--- components/Column.tsx
+++ components/Column.tsx
@@ -1,4 +1,8 @@
+import { PlusCircleIcon } from '@heroicons/react/20/solid';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import TodoCard from './TodoCard';
+import { useBoardStore } from '@/store/BoardStore';
+import { todo } from 'node:test';
 
 type Props = {
   id: TypedColumn;
@@ -15,6 +19,8 @@ const idToColumnText: {
 };
 
 function Column({ id, todos, index }: Props) {
+  const [searchString] = useBoardStore((state) => [state.searchString]);
+
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -36,32 +42,54 @@ function Column({ id, todos, index }: Props) {
                 <h2 className='flex justify-between font-bold text-xl p-2'>
                   {idToColumnText[id]}
                   <span className='text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal'>
-                    {todos.length}
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title
+                            .toLocaleLowerCase()
+                            .includes(searchString.toLocaleLowerCase())
+                        ).length}
                   </span>
                 </h2>
 
                 <div className='space-y-2'>
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.$id}
-                      draggableId={todo.$id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <TodoCard
-                          todo={todo}
-                          index={index}
-                          id={id}
-                          innerRef={provided.innerRef}
-                          draggableProps={provided.draggableProps}
-                          dragHandleProps={provided.dragHandleProps}
-                        />
-                      )}
-                    </Draggable>
-                  ))}
-                </div>
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString &&
+                      !todo.title
+                        .toLocaleLowerCase()
+                        .includes(searchString.toLocaleLowerCase())
+                    )
+                      return null;
 
-                {provided.placeholder}
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <TodoCard
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            draggableProps={provided.draggableProps}
+                            dragHandleProps={provided.dragHandleProps}
+                          />
+                        )}
+                      </Draggable>
+                    );
+                  })}
+
+                  {provided.placeholder}
+
+                  <div className='flex items-end justify-end p-2'>
+                    <button className='text-green-500 hover:text-green-600'>
+                      <PlusCircleIcon className='h-10 w-10' />
+                    </button>
+                  </div>
+                </div>
               </div>
             )}
           </Droppable>
